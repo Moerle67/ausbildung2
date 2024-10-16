a73503be5e7e076747bc39561b74a9379ed13d1b
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2024-10-16 12:35:03 +0200

    plan

--- plan/templatetags/get_aubi.py
+++ plan/templatetags/get_aubi.py
@@ -11,7 +11,12 @@ def get_aubi(temp):
     kw = int(liste[2])
     day = int(liste[3])
     daytime = Daytime.objects.get(short=liste[4].strip())
+    switch_code = liste[5]
     block = Block.objects.filter(group=group, year=year, kw=kw, day=day, daytime=daytime)
-    print("ga", len(block))
-    # print(block)
-    return block
+    if len(block) > 0:                  # mindestens ein Treffer
+        if switch_code == "aubi":
+            ds = list(block)[-1]
+            return ds.teacher   # letzter Eintrag
+    else:
+        return "--------"
+    return "error"
