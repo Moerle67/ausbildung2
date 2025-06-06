64dfacc6875886f7beb03e19a752091258a0751d
Ingo Mörl <i.moerl@bfw-schoemberg.de>
2025-06-06 11:14:16 +0200

    Lernfeld

--- lehrplan/admin.py
+++ lehrplan/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Rahmenlehrplan
+from .models import Rahmenlehrplan, Beruf, Lernfeld
 
 # Register your models here.
 admin.site.register(Rahmenlehrplan)
+admin.site.register(Beruf)
+admin.site.register(Lernfeld)
--- lehrplan/models.py
+++ lehrplan/models.py
@@ -19,3 +19,53 @@ class Rahmenlehrplan(models.Model):
     def get_absolute_url(self):
         return reverse("Daytime_detail", kwargs={"pk": self.pk})
 
+class Beruf(models.Model):
+    kuerzel = models.CharField(("Kürzel"), max_length=5)
+    name = models.CharField(("Name"), max_length=50)
+    beschreibung = models.TextField(("Beschreibung"), null=True, blank=True)
+    
+    class Meta:
+        verbose_name = ("Beruf")
+        verbose_name_plural = ("Berufe")
+
+    def __str__(self):
+        return f"{self.kuerzel} - {self.name}"
+
+    def get_absolute_url(self):
+        return reverse("Beruf_detail", kwargs={"pk": self.pk})
+
+class Lernfeld(models.Model):
+    rahmenlehrplan = models.ForeignKey(Rahmenlehrplan, verbose_name=("Rahmenlehrplan"), on_delete=models.RESTRICT)
+    nummer = models.CharField(("Nummer"), max_length=5)
+    inhalt = models.CharField(("Inhalt"), max_length=200)
+    stunden1 = models.IntegerField(("Zeitrichtwerte 1. Jahr"), default = 0)
+    stunden2 = models.IntegerField(("Zeitrichtwerte 2. Jahr"), default = 0)
+    stunden3 = models.IntegerField(("Zeitrichtwerte 3. Jahr"), default = 0)
+    berufe = models.ManyToManyField(Beruf, verbose_name=("Berufe"))
+    beschreibung = models.TextField(("Beschreibung"))
+
+
+    @property
+    def get_stunden(self):
+        return self.stunden1 + self.stunden2 + self.stunden3
+
+    @property
+    def get_berufe(self):
+        berufe = ""
+        lst_berufe = self.berufe.all()
+        for beruf in lst_berufe:
+            berufe += beruf.kuerzel + "/ "
+        return berufe[:-2]   
+         
+    class Meta:
+        verbose_name = ("Lernfeld")
+        verbose_name_plural = ("Lernfelder")
+        ordering = ["rahmenlehrplan", "nummer"]
+        
+    def __str__(self):
+        return f"{self.nummer} {self.inhalt} {self.get_berufe} ({self.get_stunden} Stunden)"
+
+    def get_absolute_url(self):
+        return reverse("Lernfeld_detail", kwargs={"pk": self.pk})
+
+
